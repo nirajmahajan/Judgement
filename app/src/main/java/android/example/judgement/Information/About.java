a4111d577a462d3f22f82856f99318f74a255c00
Niraj Mahajan <nirajmahajan007@gmail.com>
2019-06-25 04:06:21 +0530

    Added contact links in about

--- app/src/main/java/android/example/judgement/Information/About.java
+++ app/src/main/java/android/example/judgement/Information/About.java
@@ -1,9 +1,12 @@
 package android.example.judgement.Information;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.example.judgement.Game.template.GameTemplate;
 import android.example.judgement.Initialise.TemplateActivity;
 import android.example.judgement.R;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -13,6 +16,10 @@ import android.widget.TextView;
 
 public class About extends TemplateActivity {
 
+
+    private TextView gmail;
+    private TextView facebook;
+    private TextView github;
     @Override
     protected int useMenu() {
         return R.menu.done;
@@ -22,5 +29,36 @@ public class About extends TemplateActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        gmail = findViewById(R.id.mail);
+        facebook = findViewById(R.id.facebook);
+        github = findViewById(R.id.github);
     }
+
+    public void goToFb (View view) {
+        goToUrl ( "https://www.facebook.com/profile.php?id=100004470198737");
+    }
+
+    public void goToGit (View view) {
+        goToUrl ( "https://github.com/nirajmahajan");
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
+    public void sendMail(View view) {
+        Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+
+        String aEmailList[] = { "nirajm@cse.iitb.ac.in" };
+
+        emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, aEmailList);
+
+        emailIntent.setType("text/html");
+        emailIntent.setPackage("com.google.android.gm");
+        startActivity(Intent.createChooser(emailIntent, "Send mail"));
+    }
+
+
 }
