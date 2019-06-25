faa7db5db6eaff8a14e0c436c1f9b2ba0e134f4c
Niraj Mahajan <nirajmahajan007@gmail.com>
2019-06-25 10:34:29 +0530

    Created a help Template and updated the about class

--- app/src/main/java/android/example/judgement/Information/help/HelpBase.java
+++ app/src/main/java/android/example/judgement/Information/help/HelpBase.java
@@ -4,6 +4,7 @@ import android.example.judgement.Initialise.TemplateActivity;
 import android.example.judgement.R;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HelpBase extends TemplateActivity {
 
@@ -16,5 +17,49 @@ public class HelpBase extends TemplateActivity {
     @Override
     protected int useMenu() {
         return R.menu.about_exit_done;
+    }
+
+    public void howToPlay(View view) {
+
+    }
+
+    public void mode(View view) {
+
+    }
+
+    public void initPlayer(View view) {
+
+    }
+
+    public void initDealer(View view) {
+
+    }
+
+    public void takeHands(View view) {
+
+    }
+
+    public void takeResults(View view) {
+
+    }
+
+    public void scorecard(View view) {
+
+    }
+
+    public void editPlayer(View view) {
+
+    }
+
+    public void editScore(View view) {
+
+    }
+
+    public void logs(View view) {
+
+    }
+
+    public void finalMove(View view) {
+
     }
 }
---
+++ app/src/main/java/android/example/judgement/Information/help/HelpTemplate.java
@@ -0,0 +1,148 @@
+package android.example.judgement.Information.help;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.example.judgement.Information.About;
+import android.example.judgement.Initialise.MainActivity;
+import android.example.judgement.R;
+import android.example.judgement.Utils.Utils;
+import android.example.judgement.Utils.database.AppDatabase;
+import android.example.judgement.Utils.log.Utils.Log;
+import android.graphics.Color;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import java.net.Inet4Address;
+
+public class HelpTemplate extends AppCompatActivity {
+
+    private Button prev;
+    private Button next;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    public void setContentView(int layoutResID)
+    {
+        DrawerLayout fullView = (DrawerLayout) getLayoutInflater().inflate(R.layout.activity_help_template, null);
+        FrameLayout activityContainer = (FrameLayout) fullView.findViewById(R.id.help_template_activity_content);
+        getLayoutInflater().inflate(layoutResID, activityContainer, true);
+        super.setContentView(fullView);
+        prev = findViewById(R.id.help_prev);
+        next = findViewById(R.id.help_next);
+
+        prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), prevActivity());
+                startActivity(intent);
+            }
+        });
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), nextActivity());
+                startActivity(intent);
+            }
+        });
+
+        if(hideNextButton()) {
+            next.setVisibility(View.INVISIBLE);
+        }
+        if(hidePrevButton()) {
+            prev.setVisibility(View.INVISIBLE);
+        }
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (useToolbar())
+        {
+            setSupportActionBar(toolbar);
+            setTitle("Help Centre");
+            toolbar.setTitleTextColor(Color.WHITE);
+        }
+        else
+        {
+            toolbar.setVisibility(View.GONE);
+        }
+    }
+    protected boolean useToolbar() {
+        return true;
+    }
+
+    protected int useMenu() {return R.menu.about_exit_done;}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(useMenu(), menu);
+        return true;
+    }
+
+    public Toolbar getToolbar(){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        return toolbar;
+    }
+
+    protected boolean hidePrevButton() {return false;}
+    protected boolean hideNextButton() {return false;}
+
+    protected java.lang.Class nextActivity() {return MainActivity.class;}
+    protected java.lang.Class prevActivity() {return MainActivity.class;}
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menu_about) {
+            Intent intent = new Intent(getApplicationContext(), About.class);
+            startActivity(intent);
+        }else if (id == R.id.menu_done){
+            finish();
+        }
+        else if (id == R.id.menu_newGame) {
+            final Activity context = this;
+            new AlertDialog.Builder(context)
+                    .setTitle("Confirm Your Actions")
+                    .setMessage("Are you sure you want to start a new game?")
+                    .setCancelable(false)
+                    .setPositiveButton("Restart", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            AppDatabase.setAllPredictionsToReset(getApplicationContext());
+                            AppDatabase.setAllResultsToFalse(getApplicationContext());
+                            AppDatabase.setAllScoresToZero(getApplicationContext());
+                            Log.clearEditLog();
+                            Log.clearRoundLog();
+                            Intent intent = new Intent(context, MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                            dialog.cancel();
+                            startActivity(intent);
+                            finish();
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {}
+                    })
+                    .show();
+        }
+        else if (id == R.id.menu_exit) {
+            Utils.quitApp(getApplicationContext(), this);
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
